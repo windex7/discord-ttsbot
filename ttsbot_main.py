c07e6445ff5466215d00cdcc2c3d262dbd3ccfe2
windex_07 <w1ndex0721@gmail.com>
2022-08-23 15:35:18 +0900

    updated to the latest version

--- ttsbot_main.py
+++ ttsbot_main.py
@@ -1,158 +1,157 @@
 import discord
 from gtts import gTTS
 from gtts import lang
+#import AquesTalkPy
+#from AquesTalkPy import yukkuri
 import asyncio
 from mutagen.mp3 import MP3
+import wave
+import contextlib
 import re
 import json
 from langdetect import detect
 from langdetect import lang_detect_exception
 import unicodedata
 import logging
-from datetime import datetime
+import datetime
 import os
+import subprocess
+
+import ttsutil
+import yukkuriutil
 
 client = discord.Client()
 
-client_token = 'BOT TOKEN HERE'
-basedir = 'WHATEVER FOLDER NAME YOU WANT/'
-mp3dir = basedir + 'mp3/'
-logdir = basedir + 'logs/'
+client_token = 'CLIENT_TOKEN'
+mp3dir = 'mp3/'
+logdir = 'logs/'
+mp3fdir = os.getcwd() + r'\mp3'
+yukdir = os.getcwd() + r'\wav'
+msglogfilename = 'log.txt'
+msglogfiledir = logdir + msglogfilename
 
-def clear_mp3():
-	d = mp3dir
-	l = os.listdir(d)
-	for i in l:
-		try:
-			if os.path.isfile(os.path.join(d,i)):
-				os.remove(os.path.join(d,i))
-		except WindowsError:
-			pass
-
-clear_mp3()
-
-def setup_logging():
-	logger = logging.getLogger('discord')
-	logger.setLevel(logging.WARNING)
-	handler = logging.FileHandler(filename = logdir + datetime.now().strftime('%Y%m%d%H%M%S') + '.log', encoding='utf-8', mode='w')
-	handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
-	logger.addHandler(handler)
-
-setup_logging()
-
-def is_japanese(string):
-	for ch in string:
-		if ch == '\n':
-			continue
-		else:
-			name = unicodedata.name(ch) 
-			if "CJK UNIFIED" in name \
-			or "HIRAGANA" in name \
-			or "KATAKANA" in name:
-				return True
-	return False
-
-numreg = re.compile(r'^[\s0-9!-/:-@[-`{-~]+$')
-def is_num(string):
-	return numreg.match(string) is not None
-
-alphareg = re.compile(r'^[\sa-zA-Z]+$')
-def is_eng(string):
-	return alphareg.match(string) is not None
-
-halfsymbolreg = re.compile("[\r\n!-/:-@[-`{-~]")
-fullsymbolreg = re.compile(u"[︰-＠]")
-filenameheader = 'tts_'
-def convert2filename(string):
-	result = halfsymbolreg.sub("", string)
-	result = fullsymbolreg.sub("", result)
-	result = filenameheader + result
-	return result
-
-
+ttsutil.clear_mp3(mp3fdir, '.mp3')
+ttsutil.clear_mp3(yukdir, '.wav')
+ttsutil.setup_logging(logdir)
 
 @client.event
 async def on_ready():
-	print('logged in')
+    global msglogfilename
+    global msglogfiledir
+    msglogfilename = 'msg_' + datetime.datetime.now().strftime('%Y%m%d%H%M%S') + '.log'
+    msglogfiledir = logdir + msglogfilename
+    print('Logged in!')
 
 @client.event
 async def on_message(message):
-	target_server = message.server
+    if message.author.bot:
+        return
+    
+    if message.content.startswith('.help lang'):
+        reply = json.dumps(lang.tts_langs(), indent=4, sort_keys=True)
+        await message.channel.send(reply)
+        await ttsutil.delmsg(message, msglogfiledir)
+        return
 
-	if message.author.bot:
-		return
+    if message.content.startswith('.f'):
+        if message.content == '.f restart':
+            print("restarting")
+            await ttsutil.delmsg(message, msglogfiledir)
+            await client.logout()
+            exit()
+            return
+        elif message.content == '.f summon':
+            state = message.author.voice
+            if (not state) or (not state.channel):
+                await message.channel.send('the author should be in a voice channel to summon')
+                return
+            channel = state.channel
+            await channel.connect()
+            await ttsutil.delmsg(message, msglogfiledir)
+            return
+        elif message.content == '.f disconnect':
+            vc = message.guild.voice_client
+            if not vc:
+                await message.channel.send('the bot is not connected to VC')
+                return
+            await vc.disconnect()
+            await ttsutil.delmsg(message, msglogfiledir)
+            return
 
-	if message.content.startswith('.help lang'):
-		reply = json.dumps(lang.tts_langs(), indent=4, sort_keys=True)
+    if message.content.startswith('.'):
+        defaultlang = 'ja'
+        defaultnumlang = 'en'
 
-		await client.send_message(message.channel, reply)
-		return
+        """
+        if not message.author.voice:
+            await message.channel.send('the author should be in a voice channel!')
+            return
 
-	if message.content.startswith('.func'):
-		if message.content == '.func restart':
-			print("restarting")
-			await client.send_message(message.channel, 'restarting bot...')
-			await client.logout()
-			exit()
-			return
+        channel = message.author.voice.channel
+        vc = message.guild.voice_client
 
-	if message.content.startswith('.'):
-		if client.is_voice_connected(target_server):
-			tempvoice = client.voice_client_in(target_server)
-			await tempvoice.disconnect()
+        if not vc:
+            await channel.connect()
+            vc = message.guild.voice_client
+        """
+        vc = await ttsutil.get_vc(message)
+        if (vc == -1):
+            return
 
-		if message.author.voice.voice_channel == None:
-			await client.send_message(message.channel, 'messages author should be in a voice channel!')
-			return
+        msg = message.content[1:]
 
-		defaultlang = 'ja'
-		defaultnumlang = 'en'
+        result = ttsutil.process_msg(msg, defaultlang, defaultnumlang)
+        reply = result[0]
+        ttslang = result[1]
 
-		voice = await client.join_voice_channel(message.author.voice_channel)
-		msg = message.content[1:]
-		regex = r'lang:.+'
-		matchobj = re.search(regex, msg)
-		if matchobj:
-			langdic = lang.tts_langs()
-			group = matchobj.group()
-			reply = msg.replace(group, '')
-			grouplang = group.replace('lang:', '')
-			if grouplang in langdic:
-				ttslang = grouplang
-			else:
-				ttslang = defaultlang
-		else:
-			reply = msg
-			if is_num(msg):
-				await client.send_message(message.channel, 'it seems numeric. default numeric lang: ' + defaultnumlang)
-				ttslang = defaultnumlang
-			else:
-				try:
-					detectedlang = detect(msg)
-				except lang_detect_exception.LangDetectException:
-					await client.send_message(message.channel, 'could not detect lang! default lang: ' + defaultlang)
-					ttslang = defaultlang
-				else:
-					await client.send_message(message.channel, 'detected lang: ' + detectedlang)
-					if is_eng(msg):
-						ttslang = 'en'
-					elif detectedlang == 'ko':
-						if is_japanese(msg):
-							ttslang = 'ja'
-						else:
-							ttslang = detectedlang
-					else:
-						ttslang = detectedlang
-					if detectedlang != ttslang:
-						await client.send_message(message.channel, 'lang override: ' + detectedlang + ' -> ' + ttslang)
-		tts = gTTS(text=reply, lang=ttslang)
-		filename = convert2filename(reply[:10])
-		dest = mp3dir + filename + '.mp3'
-		tts.save(dest)
-		audio = MP3(dest)
-		player = voice.create_ffmpeg_player(dest, after=lambda: print('done'))
-		player.start()
-		await asyncio.sleep(audio.info.length)
-		await voice.disconnect()
+        dest = ttsutil.process_gtts(reply, ttslang, mp3dir)
+
+        #audio = discord.FFmpegPCMAudio(dest)
+        #vc.play(audio)
+        #ttsutil.playsound(vc, audio)
+        ttsutil.playsound(vc, dest)
+
+        await ttsutil.delmsg(message, msglogfiledir)
+        return
+
+    if message.content.startswith(','):
+        vc = await ttsutil.get_vc(message)
+        if (vc == -1):
+            return
+        
+        msg = message.content[1:]
+        if message.content.startswith(',,'):
+            msg = message.content[2:]
+        
+        reply = msg
+        speed = "100"
+        
+        regex = r'speed:.+'
+        matchobj = re.search(regex, msg)
+        if matchobj:
+            group = matchobj.group()
+            reply = msg.replace(group, '')
+            groupspeed = group.replace('speed:', '')
+            speed = groupspeed
+
+        if message.content.startswith(',,'):
+            reply = yukkuriutil.get_pron(reply)
+        
+        now = datetime.datetime.now()
+        filename = now.strftime('%Y%m%d%H%M%S')
+        dest = yukdir + '\\' + filename + '.wav'
+        yukkriexedest = yukdir + r'\yukkuri.exe'
+        p = subprocess.run([yukkriexedest, reply, filename, yukdir, speed], check=True)
+        if p.returncode == -1:
+            print('failed to make audio file from: ' + reply)
+            return
+        
+        #audio = discord.FFmpegPCMAudio(dest)
+        #vc.play(audio)
+        #ttsutil.playsound(vc, audio)
+        ttsutil.playsound(vc, dest)
+
+        await ttsutil.delmsg(message, msglogfiledir)
+        return
 
 client.run(client_token)
